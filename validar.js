3e0bdf4ee81e9e7e5280f3099529c42ffe9be126
De Oliveira <68508920+pedrodeoliveiraa@users.noreply.github.com>
2024-01-21 14:46:07 -0300

    Add files via upload

---
+++ validar.js
@@ -0,0 +1,11 @@
+function logar(){
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+        if(login == "admin" && senha == "admin"){
+            alert ('sucesso'); 
+            location.href = "home.html";
+        }else{
+            alert('Usuario ou Senha Incorretos');
+        }
+}
